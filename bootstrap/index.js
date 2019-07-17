579e91cc8db07748d070a02f673d7547578fdf32
dfzq <dfzq@MacBookPro.local>
2019-07-17 13:01:23 +0800

    0.0.5

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -5,15 +5,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 /*
  * @Author: shenzhiwei
  * @Date: 2019-07-06 14:11:25
  * @LastEditors: shenzhiwei
- * @LastEditTime: 2019-07-17 10:28:09
+ * @LastEditTime: 2019-07-17 10:49:38
  * @Description: 基础启动程序
  */
 /// <reference path="./fast.d.ts" />
@@ -127,10 +124,7 @@ var Bootstrap = /** @class */ (function () {
         return this;
     };
     __decorate([
-        annotationman_1.bind,
-        __metadata("design:type", Function),
-        __metadata("design:paramtypes", [Object]),
-        __metadata("design:returntype", void 0)
+        annotationman_1.bind
     ], Bootstrap.prototype, "start", null);
     return Bootstrap;
 }());
