630c521983505b3d47a38da759330dcbcb39f4ef
dfzq <dfzq@MacBookPro.local>
2019-08-08 16:25:41 +0800

    fix(location): 解决model多层还是可以被修改的缺陷
    #type: 用来描述commit的类别，只能是下面7个标识
    #     feat:新功能（feature）
    #     fix:修复bug
    #     doc:文档改变
    #     style:代码格式改变（不影响代码运行的变动，如样式、代码美化）
    #     refactor:某个已有功能重构（即不是新增功能，也不是修改bug的代码变动）
    #     test:增加测试
    #     chore:改变了build工具,如grunt换成了npm（构建过程或辅助工具的变动）
    #
    #scope: 用来说明此次修改的影响范围
    #     all:表示影响面大,如修改了网络框架会对整个程序产生影响
    #     location:表示影响小,某个小小的功能
    #     module:表示会影响某个模块,如登录模块、首页模块、用户管理模块等等
    #
    #subject: 用来简要描述本次改动的概述

--- model/index.d.ts
+++ model/index.d.ts
@@ -1,4 +1,8 @@
+declare type NestedReadonly<T> = {
+    readonly [P in keyof T]: NestedReadonly<T[P]>;
+};
 export declare class ModelService<T> {
     constructor();
-    select(): Readonly<T>;
+    select(): NestedReadonly<T>;
 }
+export {};
