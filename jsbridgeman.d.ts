1a568b9c6ebaf1428c4378c1d750d44208786fdf
dfzq <dfzq@MacBookPro.local>
2019-11-30 12:10:04 +0800

    chore(location): v2.4.3 - 新增onRequest弱类型声明
    #type: 用来描述commit的类别，只能是下面7个标识
    #     feat:新功能（feature）
    #     fix:修复bug
    #     doc:文档改变
    #     style:代码格式改变（不影响代码运行的变动，如样式、代码美化）
    #     refactor:某个已有功能重构（即不是新增功能，也不是修改bug的代码变动）
    #     test:增加测试
    #     chore:改变了build工具,如grunt换成了npm（构建过程或辅助工具的变动）
    #
    #scope: 用来说明此次修改的影响范围
    #     all:表示影响面大,如修改了网络框架会对整个程序产生影响
    #     location:表示影响小,某个小小的功能
    #     module:表示会影响某个模块,如登录模块、首页模块、用户管理模块等等
    #
    #subject: 用来简要描述本次改动的概述

--- jsbridgeman.d.ts
+++ jsbridgeman.d.ts
@@ -13,3 +13,12 @@ export function ready(callback: () => void): void;
  * @param callback 前端与客户端成功授权后，会回调callback块函数
  */
 export function oauth(callback: () => void): void;
+
+/**
+ * 调用Request指令（通常作用在一些原生和客户端之间比较特殊的操作）
+ * @param operationType 操作指令（见文档）
+ * @param payloadOptions 消息体结构
+ * @param okCallBack 成功处理回调
+ * @param notOkCallBack 失败处理回调
+ */
+export function onRequest(operationType: string, payloadOptions: any, okCallBack: (res) => void, notOkCallBack: (res) => void): void;
